7f516d7a7153f2ecd2dd620500a6d912c01e6590
witzakr <wit.zakrzewski1337@gmail.com>
2024-12-02 23:54:02 +0100

    Video section with completing the bonus task

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var video = document.getElementById('myVideo');
+    video.loop = true;     
+    video.muted = true;    
+    video.play();          
+});
