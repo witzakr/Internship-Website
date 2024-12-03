88192d24538715933bc34f652adbd95db9137f1e
witzakr <wit.zakrzewski1337@gmail.com>
2024-12-04 00:11:21 +0100

    Vendemo section start, using html tags to loop and mute the video

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var video = document.getElementById('myVideo');
-    video.loop = true;     
-    video.muted = true;    
-    video.play();          
-});
